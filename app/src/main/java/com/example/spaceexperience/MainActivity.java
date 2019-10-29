7b1746a6613f77d88e4cd3170431f58f0f0b0d83
Juan Leon <carlos25840@hotmail.com>
2019-10-29 13:39:54 +0100

    Languages

--- app/src/main/java/com/example/spaceexperience/MainActivity.java
+++ app/src/main/java/com/example/spaceexperience/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.spaceexperience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageButtonRounded buttonEn = findViewById(R.id.BtnEN);
+        final ImageButtonRounded buttonEs = findViewById(R.id.BtnES);
+        final ImageButtonRounded buttonCat = findViewById(R.id.BtnCat);
+
+        buttonEn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("en");
+            }
+        });
+
+        buttonCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("ca");
+            }
+        });
+
+        buttonEs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("");
+            }
+        });
+
+    }
+
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+       Intent refresh = new Intent(this, MainActivity.class);
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(refresh);
+        overridePendingTransition(0, 0);
     }
 }
