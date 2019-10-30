971004ef77308cbad2ac90ab75fede65706645c3
Juan Leon <carlos25840@hotmail.com>
2019-10-30 10:03:57 +0100

    fondo

--- app/src/main/java/com/example/spaceexperience/MainActivity.java
+++ app/src/main/java/com/example/spaceexperience/MainActivity.java
@@ -14,14 +14,14 @@ import android.widget.ImageButton;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
     private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         final ImageButtonRounded buttonEn = findViewById(R.id.BtnEN);
