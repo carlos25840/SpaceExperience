396372b37a5b06cb306235bce27374cd9939ba81
Juan Leon <carlos25840@hotmail.com>
2019-10-31 11:33:49 +0100

    Nueva activity niveles adulto

--- app/src/main/java/com/example/spaceexperience/AdultActivity.java
+++ app/src/main/java/com/example/spaceexperience/AdultActivity.java
@@ -2,31 +2,19 @@ package com.example.spaceexperience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class NivelActivity extends AppCompatActivity {
+public class AdultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_nivel);
-
+        setContentView(R.layout.activity_adult);
         ImageButton btnBack = findViewById(R.id.BtnBack);
-        Button btnAdult = findViewById(R.id.btnAdulto);
-
-        btnAdult.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(NivelActivity.this, AdultActivity.class);
-                startActivity(intent);
-            }
-        });
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
