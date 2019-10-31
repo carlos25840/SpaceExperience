f53de3aef13f77d01063cc23d3f6d552948b6f01
Juan Leon <carlos25840@hotmail.com>
2019-10-31 10:52:26 +0100

    Nueva activity

--- app/src/main/java/com/example/spaceexperience/MainActivity.java
+++ app/src/main/java/com/example/spaceexperience/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.spaceexperience;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 import java.util.Locale;
 
@@ -19,13 +19,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         final ImageButtonRounded buttonEn = findViewById(R.id.BtnEN);
         final ImageButtonRounded buttonEs = findViewById(R.id.BtnES);
         final ImageButtonRounded buttonCat = findViewById(R.id.BtnCat);
+
+        final Button btnPlay = findViewById(R.id.BtnJugar);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NivelActivity.class);
+                startActivity(intent);
+            }
+        });
 
         buttonEn.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/spaceexperience/NivelActivity.java
@@ -0,0 +1,35 @@
+package com.example.spaceexperience;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class NivelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_nivel);
+
+        ImageButton btnBack = findViewById(R.id.BtnBack);
+        Button btnAdult = findViewById(R.id.btnAdulto);
+
+        btnAdult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
