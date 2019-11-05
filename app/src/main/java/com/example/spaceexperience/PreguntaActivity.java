73dd4bc8cd512671778d7e3d92820b72e3ff1fc0
María José Benites Gaitán <mariajosebg95@gmail.com>
2019-11-05 14:00:36 +0100

    Puntos y tiempo

--- app/src/main/java/com/example/spaceexperience/PreguntaActivity.java
+++ app/src/main/java/com/example/spaceexperience/PreguntaActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -12,56 +13,53 @@ import android.widget.TextView;
 
 public class PreguntaActivity extends AppCompatActivity {
 
-    private int score2;
-    private boolean time = true;
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_pregunta);
-
         final TextView textViewCounter = findViewById(R.id.counter);
         final TextView textViewPuntos = findViewById(R.id.txtPuntos);
-        Button buttonRespuesta1 = findViewById(R.id.btnRespuesta1);
+        final Button buttonRespuesta1 = findViewById(R.id.btnRespuesta1);
         Button buttonRespuesta2 = findViewById(R.id.btnRespuesta2);
         Button buttonRespuesta3 = findViewById(R.id.btnRespuesta3);
         Button buttonRespuesta4 = findViewById(R.id.btnRespuesta4);
 
-        Intent intent = getIntent();
-        int score = intent.getIntExtra("score", 0);
-        score2 = score;
-        textViewPuntos.setText(Integer.toString(score));
-
-        buttonRespuesta1.setOnClickListener(new View.OnClickListener() {
+        final CountDownTimer timer = new CountDownTimer(15000, 1000) {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PreguntaActivity.this, PreguntaActivity.class);
-                if (time)
-                {
-                    score2 +=(2*Integer.parseInt(textViewCounter.getText().toString()));
-                }
-                else
-                {
-                    score2-=5;
-                }
-                intent.putExtra("score", score2);
-                startActivity(intent);
-            }
-        });
-
-        new CountDownTimer (15000, 1000) {
-
             public void onTick(long millisUntilFinished) {
                 textViewCounter.setText("" + millisUntilFinished / 1000);
             }
 
+            @Override
             public void onFinish() {
-                textViewCounter.setTextColor(getResources().getColor(R.color.rojo));
-                textViewCounter.setText(R.string.timeOut);
-                time = false;
+                score -= 5;
+                textViewPuntos.setText(Integer.toString(score));
+                this.cancel();
+                this.start();
             }
-        }.start();
+        };
+
+        timer.start();
+
+        final Intent intent = getIntent();
+        score = intent.getIntExtra("score", 0);
+        textViewPuntos.setText(Integer.toString(score));
+        buttonRespuesta1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PreguntaActivity.this, PreguntaActivity.class);
+                if(Integer.parseInt(textViewCounter.getText().toString()) >= 1)
+                {
+                    score += 2*(Integer.parseInt(textViewCounter.getText().toString()));
+                    textViewPuntos.setText(Integer.toString(score));
+                    timer.cancel();
+                    timer.start();
+                }
+            }
+        });
 
     }
 
