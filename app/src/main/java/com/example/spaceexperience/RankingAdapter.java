8370c8493911e7a94f99fbc64ca1061914f6ecea
María José Benites Gaitán <mariajosebg95@gmail.com>
2019-12-03 14:00:45 +0100

    Limpieza código y comentarios

--- app/src/main/java/com/example/spaceexperience/RankingAdapter.java
+++ app/src/main/java/com/example/spaceexperience/RankingAdapter.java
@@ -8,12 +8,13 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import java.util.ArrayList;
 import JSONs.Resultado;
-
+//Constructor personalizado que hereda de ArrayAdapter
 public class RankingAdapter extends ArrayAdapter<Resultado> {
-    Context context;
-    ArrayList<Resultado> resultados;
-
+    private Context context;
+    private ArrayList<Resultado> resultados;
+    //Constructor al que le pasamos el contexto y un ArrayList del tipo Resultado
     public RankingAdapter(Context context, ArrayList<Resultado> resultados) {
+        //Se pasa al constructor super, el contexto recibido, el layout personalizado y el ArrayList de resultadps
         super(context, R.layout.ranking_player, resultados);
         this.context = context;
         this.resultados = resultados;
@@ -23,16 +24,19 @@ public class RankingAdapter extends ArrayAdapter<Resultado> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View item = inflater.inflate(R.layout.ranking_player,null);
+        //Views
         TextView txtNombre = item.findViewById(R.id.TxtName);
         TextView txtPuntos = item.findViewById(R.id.TxtPoints);
         TextView txtInsignias = item.findViewById(R.id.TxtInsignias);
         TextView txtPos = item.findViewById(R.id.TxtPos);
-        int pos = position +1;
-        txtPos.setText(Integer.toString(pos));
 
+        //Para llenar las posiciones del ranking se incrementa la position en 1 para no empezar el ranking desde 0
+        txtPos.setText(Integer.toString(position +1));
+
+        //Se van cargando los TextView del layout personalizado
         txtNombre.setText(resultados.get(position).getNombre());
         txtInsignias.setText(Integer.toString(resultados.get(position).getInsignias()));
         txtPuntos.setText(Integer.toString(resultados.get(position).getPuntacion()));
-        return (item);
+        return item;
     }
 }
